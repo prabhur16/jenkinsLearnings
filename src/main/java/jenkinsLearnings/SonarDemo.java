ea3354678bb7d374d1cc230d4c890a2619abeda9
“prabhu” <prabhur16@gmail.com>
2020-03-22 22:14:24 +0530

    Initial Commit

---
+++ src/main/java/jenkinsLearnings/SonarDemo.java
@@ -0,0 +1,14 @@
+package jenkinsLearnings;
+
+public class SonarDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Same program for Sonar Integration Demo
+		
+		System.out.println("SonarQube Scanner");
+		
+	}
+
+}
